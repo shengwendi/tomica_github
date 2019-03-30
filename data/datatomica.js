dd25403bcd0cdb6804f376c5a9c3d0f68237b016
shengwendi <shengwendi@gmail.com>
2019-03-30 22:56:21 +0800

    add pages

---
+++ data/datatomica.js
@@ -0,0 +1,38 @@
+var datatomica = [
+  {
+    "201905": [
+      {
+        "トミカシリーズ": [
+          {
+            "carName": "No.49 トヨタ コースター ようちえんバス",
+            "carPrice": "2019年5月発売予定 メーカー希望小売価格 450円(税抜)",
+            "carImg": "https://www.takaratomy.co.jp/products/tomica/new/images/1905/049.jpg",
+            "seriesMarks": "https://www.takaratomy.co.jp/products/tomica/images_2016/common/mark/logo-tomica-day.png",
+            "carInfo": "トヨタ コースター ようちえんバス がトミカになって登場！"
+          },
+          {
+            "carName": "No.115 スバル フォレスター",
+            "carPrice": "2019年5月発売予定 メーカー希望小売価格 450円(税抜)",
+            "carImg": "https://www.takaratomy.co.jp/products/tomica/new/images/1905/115.jpg",
+            "seriesMarks": "https://www.takaratomy.co.jp/products/tomica/images_2016/common/mark/logo-tomica-day.png",
+            "carInfo": "スバル フォレスターがトミカになって登場！\nサスペンション"
+          }
+        ]
+      },
+      {
+        "ドリームトミカ": [
+          {
+            "carName": "ドリームトミカ NO.149 すみっコぐらし ぺんぎん？のアイスクリーム屋さん",
+            "carPrice": "2019年5月発売予定 メーカー希望小売価格 700円(税抜)",
+            "carImg": "https://www.takaratomy.co.jp/products/tomica/new/images/1905/dt_149_smk_01.jpg",
+            "seriesMarks": "",
+            "carInfo": "すみっコぐらしが、アイスクリームワゴン車をモチーフにしたかわいいデザインになってドリームトミカに登場！\nサスペンション、カウンター開閉\n© 2019 San-X Co., Ltd. All Rights Reserved."
+          }
+        ]
+      }
+    ]
+  }
+]
+module.exports = {
+  tomicalist: datatomica
+}
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,15 @@
 //index.js
 //获取应用实例
 const app = getApp()
+var datatomica = require('../../data/datatomica.js');
 
 Page({
   data: {
 
   },
   onLoad: function () {
-
+    this.setData({
+      datatomica: datatomica.tomicalist
+    })
   }
 })
