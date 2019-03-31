94331a4dfda49e1661727cbce484d6704f4ca9df
shengwendi <shengwendi@gmail.com>
2019-03-31 12:21:43 +0800

    add data

--- data/datatomica.js
+++ data/datatomica.js
@@ -1,37 +1,83 @@
 var datatomica = [
-  {
-    "201905": [
-      {
-        "トミカシリーズ": [
-          {
-            "carName": "No.49 トヨタ コースター ようちえんバス",
-            "carPrice": "2019年5月発売予定 メーカー希望小売価格 450円(税抜)",
-            "carImg": "https://www.takaratomy.co.jp/products/tomica/new/images/1905/049.jpg",
-            "seriesMarks": "https://www.takaratomy.co.jp/products/tomica/images_2016/common/mark/logo-tomica-day.png",
-            "carInfo": "トヨタ コースター ようちえんバス がトミカになって登場！"
-          },
-          {
-            "carName": "No.115 スバル フォレスター",
-            "carPrice": "2019年5月発売予定 メーカー希望小売価格 450円(税抜)",
-            "carImg": "https://www.takaratomy.co.jp/products/tomica/new/images/1905/115.jpg",
-            "seriesMarks": "https://www.takaratomy.co.jp/products/tomica/images_2016/common/mark/logo-tomica-day.png",
-            "carInfo": "スバル フォレスターがトミカになって登場！\nサスペンション"
-          }
-        ]
-      },
-      {
-        "ドリームトミカ": [
-          {
-            "carName": "ドリームトミカ NO.149 すみっコぐらし ぺんぎん？のアイスクリーム屋さん",
-            "carPrice": "2019年5月発売予定 メーカー希望小売価格 700円(税抜)",
-            "carImg": "https://www.takaratomy.co.jp/products/tomica/new/images/1905/dt_149_smk_01.jpg",
-            "seriesMarks": "",
-            "carInfo": "すみっコぐらしが、アイスクリームワゴン車をモチーフにしたかわいいデザインになってドリームトミカに登場！\nサスペンション、カウンター開閉\n© 2019 San-X Co., Ltd. All Rights Reserved."
-          }
-        ]
-      }
-    ]
-  }
+  // 201905
+  [
+    {
+      "carName": "No.49 トヨタ コースター ようちえんバス",
+      "carPrice": "2019年5月発売予定 メーカー希望小売価格 450円(税抜)",
+      "carImg": "https://www.takaratomy.co.jp/products/tomica/new/images/1905/049.jpg",
+      "carInfo": "トヨタ コースター ようちえんバス がトミカになって登場！"
+    },
+    {
+      "carName": "No.115 スバル フォレスター",
+      "carPrice": "2019年5月発売予定 メーカー希望小売価格 450円(税抜)",
+      "carImg": "https://www.takaratomy.co.jp/products/tomica/new/images/1905/115.jpg",
+      "carInfo": "スバル フォレスターがトミカになって登場！\nサスペンション"
+    },
+    {
+      "carName": "ドリームトミカ NO.149 すみっコぐらし ぺんぎん？のアイスクリーム屋さん",
+      "carPrice": "2019年5月発売予定 メーカー希望小売価格 700円(税抜)",
+      "carImg": "https://www.takaratomy.co.jp/products/tomica/new/images/1905/dt_149_smk_01.jpg",
+      "carInfo": "すみっコぐらしが、アイスクリームワゴン車をモチーフにしたかわいいデザインになってドリームトミカに登場！\nサスペンション、カウンター開閉\n© 2019 San-X Co., Ltd. All Rights Reserved."
+    }
+  ],
+  // 201904
+  [
+    {
+      "carName": "No.14 スズキ ジムニー",
+      "carPrice": "2019年4月発売予定 メーカー希望小売価格 450円(税抜)",
+      "carImg": "https://www.takaratomy.co.jp/products/tomica/new/images/1904/014.jpg",
+      "carInfo": "スズキ ジムニーがトミカになって登場！\nサスペンション"
+    },
+    {
+      "carName": "No.28 いすゞ サインカー",
+      "carPrice": "2019年4月発売予定 メーカー希望小売価格 450円(税抜)",
+      "carImg": "https://www.takaratomy.co.jp/products/tomica/new/images/1904/028.jpg",
+      "carInfo": "いすゞ サインカーがトミカになって登場！\nサスペンション／掲示板回転、上下"
+    },
+    {
+      "carName": "ビッグに変形！デカパトロールカー",
+      "carPrice": "2019年4月発売予定 メーカー希望小売価格 6,200円(税抜)",
+      "carImg": "https://www.takaratomy.co.jp/products/tomica/new/images/1904/tw_policebase.jpg",
+      "carInfo": "警察署にドデカく変形！トミカを最大12台積載可能な大きなパトロールカーが登場！\n【パトロールカーモード】\nボタンを押すとパトライトが光りボイスやサイレン音やが鳴ります。トミカを最大12台積載可能。\n【ポリスベースモード】\nフロント、ボンネットを開いて、変形ボタンを押すとサウンドとともに大きな警察署に変形！待機していたトミカが変形と同時に一斉出動！出動ボタンを押すと中央指令室に待機していたトミカがサウンドとともに迫力の緊急出動だ！\n他にも発車ガレージやメンテナンスガレージや給油ガレージなど基地ギミックを搭載。\n【さらに変形ファイヤーステーションと合体！】\n別売りの変形ファイヤーステーションのスパイラルスロープからデカパトロールカーの後部ハッチを連結させることで最大10台以上のトミカを一斉発車させて遊ぶことができます。※変形ファイヤーステーション（別売り）と連結する合体スロープ付属。\nアルカリ乾電池単４☓2個使用(別売)"
+    },
+    {
+      "carName": "",
+      "carPrice": "",
+      "carImg": "",
+      "carInfo": ""
+    },
+    {
+      "carName": "",
+      "carPrice": "",
+      "carImg": "",
+      "carInfo": ""
+    },
+    {
+      "carName": "",
+      "carPrice": "",
+      "carImg": "",
+      "carInfo": ""
+    },
+    {
+      "carName": "",
+      "carPrice": "",
+      "carImg": "",
+      "carInfo": ""
+    },
+    {
+      "carName": "",
+      "carPrice": "",
+      "carImg": "",
+      "carInfo": ""
+    },
+    {
+      "carName": "",
+      "carPrice": "",
+      "carImg": "",
+      "carInfo": ""
+    }
+  ]
+  
 ]
 module.exports = {
   tomicalist: datatomica
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,15 @@ var datatomica = require('../../data/datatomica.js');
 
 Page({
   data: {
-
+    // selectDate: null,
+    array: ['2019年05月発売', '2019年04月発売', '2019年03月発売', '2019年02月発売'],
+    index: 0,
+  },
+  bindPickerChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
   },
   onLoad: function () {
     this.setData({
