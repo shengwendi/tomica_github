d4e27910f03a32d2c86c48bb2a76ee3296d2af6b
shengwendi <shengwendi@gmail.com>
2019-04-05 16:29:50 +0800

    修改样式

--- pages/tlvneo/tlvneo.js
+++ pages/tlvneo/tlvneo.js
@@ -5,11 +5,17 @@ var datatlvneo = require('../../data/datatlvneo.js');
 
 Page({
   data: {
-    listNum: 12
+    listNum: 12, // 每次加几辆车
+    len: 0, // 数组长度
+    over: false // 判断是否全部加载完成
   },
   onLoad: function () {
     this.initNum(this.data.listNum);
     wx.showShareMenu(); // 转发
+
+    this.setData({
+      len: datatlvneo.tlvneolist.length // 数组的长度
+    });
   },
   // 加载数据方法
   initNum: function(num) {
@@ -20,7 +26,18 @@ Page({
   },
   // 加载更多
   loadMore: function () {
-    this.data.listNum += 12;
-    this.initNum(this.data.listNum);
+    if (this.data.listNum <= this.data.len) {
+      this.data.listNum += 12;
+      this.initNum(this.data.listNum);
+    } else {
+      wx.showToast({
+        title: '没有更多啦~',
+        icon: 'none',
+        duration: 2000
+      })
+      this.setData({
+        over: true
+      })
+    }
   }
 })
