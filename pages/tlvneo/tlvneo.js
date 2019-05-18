fc594afefa8c6010ee0dee4d2273331201ccebcc
shengwendi <shengwendi@gmail.com>
2019-05-18 15:02:20 +0800

    feat: add search

--- pages/tlvneo/tlvneo.js
+++ pages/tlvneo/tlvneo.js
@@ -39,5 +39,11 @@ Page({
         over: true
       })
     }
+  },
+  // 跳转到搜索页面
+  jumpSearch: function () {
+    wx.navigateTo({
+      url: 'tlvneoSearch'
+    });
   }
 })
---
+++ pages/tlvneo/tlvneoSearch.js
@@ -0,0 +1,47 @@
+// pages/tlvneo/tlvneoSearch.js
+var datatlvneo = require('../../data/datatlvneo.js');
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    searchValue: '', // input value
+    searchList: [], // 搜索结果列表
+    resultEmp: false, // 搜索结果
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.showShareMenu(); // 转发
+  },
+
+  // 获取input value值
+  searchValueInput: function(e) {
+    let value = e.detail.value;
+    this.setData({
+      searchValue: value,
+    });
+  },
+  // 搜索
+  funSearch: function(e) {
+    let keywords = e.currentTarget.dataset.keywords.trim();
+    let allCar = datatlvneo.tlvneolist; // 获取所有的车
+    let searchList = allCar.filter((item) => {
+      return item.specTitle.toLowerCase().match(keywords.toLowerCase());
+    });
+    this.setData({
+      resultEmp: false,
+      searchList: searchList
+    });
+    // 无结果
+    if (!searchList.length) {
+      this.setData({
+        resultEmp: true
+      })
+    }
+  },
+})
