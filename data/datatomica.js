471e9799e6b03d98091cdc4ca6c0fd60e6aaacfb
shengwendi <shengwendi@gmail.com>
2019-06-25 13:31:50 +0800

    feat: add 7 data

--- data/datatomica.js
+++ data/datatomica.js
@@ -106,6 +106,12 @@ var datatomica = [
       "carPrice": "2019年7月20日(土)発売予定 メーカー希望小売価格 700円(税抜)",
       "carImg": "https://www.takaratomy.co.jp/products/tomica/new/images/1907/so_toysrus.jpg",
       "carInfo": "今回のトイザらス オリジナルトミカは、スズキ スイフトスポーツ カスタマイズタイプが登場！\nサスペンション／後部ドア開閉"
+    },
+    {
+      "carName": "TCNオリジナル ダンロップ レーシングトランスポーター",
+      "carPrice": "2019年7月27日(土)発売予定 メーカー希望小売価格 700円(税抜)",
+      "carImg": "https://www.takaratomy.co.jp/products/tomica/new/images/1907/so_tcnn.jpg",
+      "carInfo": "ダンロップ レーシングトランスポーターがTCNオリジナルとして登場！\n後部パネル開閉"
     }
   ],
   // 201906
